4de86b2e4aca2503c01df18b87a6346f841dd78d
nuret209 <noguc4200@gmail.com>
2024-11-10 23:00:00 +0300

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,14 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Menu from "@/components/Menu";
 import Apps from "@/components/Apps";
+import Head from "next/head";
+import Link from "next/link";
 export const metadata: Metadata = {
   title: "Nurettin Öğüç",
   description: "Nurettin Öğüç Personal Website",
+  icons: {
+    icon: "/images/favicon.ico",
+  }
 };
 
 export default function RootLayout({
@@ -15,10 +20,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/images/favicon.ico" sizes="any" />
+      </Head>
       <body
         className={`antialiased relative`}
       >
-
         <div className=" root relative h-full">
           <Menu />
           <Apps />
